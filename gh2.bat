8c9f1cfd7b92384a983495e64efc3cdd233295de
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-04-15 07:54:48 -0500

    move ark files to _ark. optimize build

--- gh2.bat
+++ gh2.bat
@@ -1,7 +1,2 @@
-del /f %~dp0\gen\main.hdr
-del /f %~dp0\gen\main_0.ark
-del /f %~dp0\gen\main_1.ark
-arkhelper dir2ark %~dp0\gen %~dp0\build -e -s 4073741823
-move %~dp0\build\main.hdr %~dp0\gen\main.hdr
-move %~dp0\build\main_0.ark %~dp0\gen\main_0.ark
+arkhelper dir2ark %~dp0\_ark %~dp0\gen -e -s 4073741823
 xenia_canary.exe default.xex
