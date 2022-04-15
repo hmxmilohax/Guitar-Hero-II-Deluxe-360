6228551050b4ed4bbab86597d1ba5902954509c5
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-04-14 21:33:48 -0500

    portable gh2

---
+++ gh2.bat
@@ -0,0 +1,7 @@
+del /f %~dp0\gen\main.hdr
+del /f %~dp0\gen\main_0.ark
+del /f %~dp0\gen\main_1.ark
+arkhelper dir2ark %~dp0\gen %~dp0\build -e -s 4073741823
+mv %~dp0\build\main.hdr %~dp0\gen\main.hdr
+mv %~dp0\build\main_0.ark %~dp0\gen\main_0.ark
+xenia_canary.exe default.xex
