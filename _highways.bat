f33a24a074cc0a370a92beada1c27f8842f4a373
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-04-15 16:42:09 -0500

    cleanup, idk what's wrong

--- _highways.bat
+++ _highways.bat
@@ -1,11 +1,12 @@
 del /f %~dp0highways\highways.dta
-cd %~dp0/highways
+cd %~dp0highways
 forfiles /s /m *.* /C "cmd /e:on /v:on /c set \"Phile=@file\" & if @ISDIR==FALSE ren @file !Phile: =_!"
-forfiles /s /C "cmd /e:on /v:on /c set \"Phile=@file\" & if @ISDIR==TRUE ren @file !Phile: =_!"
+forfiles /s /m *.* /C "cmd /e:on /v:on /c set \"Phile=@file\" & if @ISDIR==FALSE  ren @file !Phile:_=-!"
 for %%i in (*.bmp) do @echo "%%~ni">> highways.dta
 for %%i in (*.png) do @echo "%%~ni">> highways.dta
 for %%i in (*.jpg) do @echo "%%~ni">> highways.dta
-mv %~dp0\highways\highways.dta %~dp0_ark\track\surfaces\highways.dta
-FOR /F "tokens=*" %%G IN ('dir /b *.jpg') DO superfreq png2tex "%%G" "%~dp0\_ark\track\surfaces\gen\%%~nG_keep.bmp_xbox" --miloVersion 25 --preset gh2_x360
-FOR /F "tokens=*" %%G IN ('dir /b *.png') DO superfreq png2tex "%%G" "%~dp0\_ark\track\surfaces\gen\%%~nG_keep.bmp_xbox" --miloVersion 25 --preset gh2_x360
-FOR /F "tokens=*" %%G IN ('dir /b *.bmp') DO superfreq png2tex "%%G" "%~dp0\_ark\track\surfaces\gen\%%~nG_keep.bmp_xbox" --miloVersion 25 --preset gh2_x360
+mv %~dp0highways\highways.dta %~dp0_ark\track\surfaces\highways.dta
+FOR /F "tokens=*" %%G IN ('dir /b *.png') DO %~dp0magick/magick.exe convert "%~dp0highways/%%G" -resize 512x1024 -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.jpg') DO %~dp0magick/magick.exe convert "%~dp0highways/%%G" -resize 512x1024 -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.bmp') DO %~dp0magick/magick.exe convert "%~dp0highways/%%G" -resize 512x1024 -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.png') DO %~dp0dependencies/superfreq.exe png2tex "%~dp0highways/%%G" "%~dp0\_ark\track\surfaces\gen\%%~nG_keep.bmp_xbox" --platform x360 --miloVersion 25
--- gh2.bat
+++ gh2.bat
@@ -1,2 +1,2 @@
-arkhelper dir2ark %~dp0\_ark %~dp0\gen -e -s 4073741823
-xenia_canary.exe default.xex
+%~dp0dependencies/arkhelper dir2ark %~dp0\_ark %~dp0\gen -e -s 4073741823
+%~dp0xenia_canary.exe %~dp0default.xex
