d0255b8988c35ef1a7d47493392130f54acc6f6a
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-04-16 13:46:22 -0500

    move magick to dependencies

--- _highways.bat
+++ _highways.bat
@@ -7,7 +7,7 @@ for %%i in (*.bmp) do @echo "%%~ni">> highways.dta
 for %%i in (*.png) do @echo "%%~ni">> highways.dta
 for %%i in (*.jpg) do @echo "%%~ni">> highways.dta
 move "%~dp0highways\highways.dta" "%~dp0_ark\track\surfaces\highways.dta"
-FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0magick/magick.exe" convert "%~dp0highways/%%G" -resize 512x1024! -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
-FOR /F "tokens=*" %%G IN ('dir /b *.jpg') DO "%~dp0magick/magick.exe" convert "%~dp0highways/%%G" -resize 512x1024! -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
-FOR /F "tokens=*" %%G IN ('dir /b *.bmp') DO "%~dp0magick/magick.exe" convert "%~dp0highways/%%G" -resize 512x1024! -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0dependencies/magick/magick.exe" convert "%~dp0highways/%%G" -resize 512x1024! -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.jpg') DO "%~dp0dependencies/magick/magick.exe" convert "%~dp0highways/%%G" -resize 512x1024! -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.bmp') DO "%~dp0dependencies/magick/magick.exe" convert "%~dp0highways/%%G" -resize 512x1024! -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
 FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0dependencies/superfreq.exe" png2tex "%~dp0highways/%%G" "%~dp0_ark\track\surfaces\gen\%%~nG_keep.bmp_xbox" --platform x360 --miloVersion 25
