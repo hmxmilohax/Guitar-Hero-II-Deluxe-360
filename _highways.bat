57adbd2565969ab8a7ec66ad04a15a5ea06f0118
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-04-16 09:29:06 -0500

    format

--- _highways.bat
+++ _highways.bat
@@ -10,4 +10,4 @@ move "%~dp0highways\highways.dta" "%~dp0_ark\track\surfaces\highways.dta"
 FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0magick/magick.exe" convert "%~dp0highways/%%G" -resize 512x1024! -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
 FOR /F "tokens=*" %%G IN ('dir /b *.jpg') DO "%~dp0magick/magick.exe" convert "%~dp0highways/%%G" -resize 512x1024! -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
 FOR /F "tokens=*" %%G IN ('dir /b *.bmp') DO "%~dp0magick/magick.exe" convert "%~dp0highways/%%G" -resize 512x1024! -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
-FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0dependencies/superfreq.exe" png2tex "%~dp0highways/%%G" "%~dp0\_ark\track\surfaces\gen\%%~nG_keep.bmp_xbox" --platform x360 --miloVersion 25
+FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0dependencies/superfreq.exe" png2tex "%~dp0highways/%%G" "%~dp0_ark\track\surfaces\gen\%%~nG_keep.bmp_xbox" --platform x360 --miloVersion 25
