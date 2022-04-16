6b7b229065ea360160bd5b17617b340ecc15790a
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-04-15 22:34:41 -0500

    oops

--- _highways.bat
+++ _highways.bat
@@ -6,7 +6,7 @@ for /f "Tokens=*" %%f in ('dir /l/b/a-d/s') do (move /y "%%f" "%%f")
 for %%i in (*.bmp) do @echo "%%~ni">> highways.dta
 for %%i in (*.png) do @echo "%%~ni">> highways.dta
 for %%i in (*.jpg) do @echo "%%~ni">> highways.dta
-move "%~dp0highways\highways.dta %~dp0_ark\track\surfaces\highways.dta"
+move "%~dp0highways\highways.dta" "%~dp0_ark\track\surfaces\highways.dta"
 FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0magick/magick.exe" convert "%~dp0highways/%%G" -resize 512x1024! -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
 FOR /F "tokens=*" %%G IN ('dir /b *.jpg') DO "%~dp0magick/magick.exe" convert "%~dp0highways/%%G" -resize 512x1024! -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
 FOR /F "tokens=*" %%G IN ('dir /b *.bmp') DO "%~dp0magick/magick.exe" convert "%~dp0highways/%%G" -resize 512x1024! -filter Box -define dither:diffusion-amount=50%% -dither FloydSteinberg -colors 256 -depth 8 "%~dp0highways\%%~nG.png"
