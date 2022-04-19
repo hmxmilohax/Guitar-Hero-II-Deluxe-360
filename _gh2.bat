6f07ef61d7c60c91cd757931996e27f3ecc85e44
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-04-18 22:11:30 -0500

    Update _gh2.bat

--- _gh2.bat
+++ _gh2.bat
@@ -1,3 +1,3 @@
-git pull
+git pull https://github.com/LlysiX/Guitar-Hero-II-Deluxe-360 main
 "%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\gen" -e -s 4073741823
 "%~dp0xenia_canary.exe" "%~dp0default.xex"
