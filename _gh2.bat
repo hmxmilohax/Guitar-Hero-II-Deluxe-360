57c15e1df8d364171ac8b35bdfd97659b303ef98
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-09-10 19:43:56 -0500

    update links

--- _gh2.bat
+++ _gh2.bat
@@ -1,4 +1,4 @@
-git pull https://github.com/LlysiX/Guitar-Hero-II-Deluxe-360 main
+git pull https://github.com/hmxmilohax/Guitar-Hero-II-Deluxe-360 main
 "%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\gen" -e -s 4073741823
 "%~dp0xenia_canary.exe" "%~dp0default.xex"
 pause
--- _reset.bat
+++ _reset.bat
@@ -1,3 +1,3 @@
-git fetch https://github.com/LlysiX/Guitar-Hero-II-Deluxe-360 main
+git fetch https://github.com/hmxmilohax/Guitar-Hero-II-Deluxe-360 main
 git reset --hard origin/main
 pause
