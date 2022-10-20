337a4adaf414ded62f00b510719d6f88a55945e8
LysiX <79173634+LlysiX@users.noreply.github.com>
2022-10-19 17:37:23 -0700

    slightly better noflames

--- _textures-custom_track_textures.bat
+++ _textures-custom_track_textures.bat
@@ -1,3 +1,3 @@
-cd "%~custom_track_textures"
+cd "%~dp0custom_track_textures"
 FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0dependencies/superfreq.exe" png2tex "%~dp0custom_track_textures/%%G" "%~dp0_ark\track\custom_track_textures\gen\%%~nG_keep.png_xbox" --platform x360 --miloVersion 25
 pause
