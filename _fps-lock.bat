aa62de23c8007f9caa166e4b09fec7ab21267d25
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-11-08 18:11:45 -0600

    update xenia config. LOCK FPS, add unlock script

---
+++ _fps-lock.bat
@@ -0,0 +1,2 @@
+del xenia-canary.config.toml
+copy %~dp0xenia-canary_vsync-on.config.toml %~dp0xenia-canary.config.toml
---
+++ _fps-unlock.bat
@@ -0,0 +1,2 @@
+del xenia-canary.config.toml
+copy %~dp0xenia-canary_vsync-off.config.toml %~dp0xenia-canary.config.toml
