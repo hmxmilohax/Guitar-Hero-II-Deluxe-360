aac2180b33e5cbabfbaa311c77946dc4afec3d93
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-11-17 18:11:29 -0600

    remove fps unlock and lock scripts for now

--- _fps-lock.bat
+++
@@ -1,2 +0,0 @@
-del xenia-canary.config.toml
-copy %~dp0xenia-canary_vsync-on.config.toml %~dp0xenia-canary.config.toml
--- _fps-unlock.bat
+++
@@ -1,2 +0,0 @@
-del xenia-canary.config.toml
-copy %~dp0xenia-canary_vsync-off.config.toml %~dp0xenia-canary.config.toml
