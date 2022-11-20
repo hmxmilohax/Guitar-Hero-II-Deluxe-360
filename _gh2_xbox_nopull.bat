7909d0f1fc973cbbdf88c0813d20f1606fcae807
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-11-20 17:22:54 -0600

    fix build scripts for xbox

--- _gh2_xbox_nopull.bat
+++ _gh2_xbox_nopull.bat
@@ -1,3 +1,3 @@
 rem git pull https://github.com/hmxmilohax/Guitar-Hero-II-Deluxe-360 main
-"%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\gen" -e -s 4073741823
+"%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\Xbox\gen" -e -s 4073741823
 pause
