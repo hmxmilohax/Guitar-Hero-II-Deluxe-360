778038dc5526752e211a57506a26c51e109e2f38
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-12-13 16:19:51 -0600

    copy default image to album_art folder if doesn't exist

--- _gh2_xbox.bat
+++ _gh2_xbox.bat
@@ -1,3 +1,5 @@
 git pull https://github.com/hmxmilohax/Guitar-Hero-II-Deluxe-360 main
+for /f "tokens=*" %%D in ('dir /b /s /a:d "%~dp0\content\*"') do (for /f "delims=" %%A in ('dir /s /b "%%D\gen\*.bmp_xbox"') do mkdir "%~dp0_build\Xbox\gen\album_art\%%~nD" && mkdir "%~dp0_build\Xbox\gen\album_art\%%~nD\gen" && xcopy "%%D\gen\*.bmp_xbox" "%~dp0_build\Xbox\gen\album_art\%%~nD\gen" /Y)
+for /f "tokens=*" %%D in ('dir /b /s /a:d "%~dp0\content\*"') do (for /f "delims=" %%A in ('dir /s /b "%%D\*.mid"') do mkdir "%~dp0_build\Xbox\gen\album_art\%%~nD" && mkdir "%~dp0_build\Xbox\gen\album_art\%%~nD\gen" && copy /b "_ark\ui\image\ng\gen\photo_random0_keep.bmp_xbox" "%~dp0_build\Xbox\gen\album_art\%%~nD\gen\%%~nD.bmp_xbox" /N)
 "%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\Xbox\gen" -e -s 4073741823
 pause
