f36c62ae68e0ac3ef40498e9a74d83dd5039acae
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-12-13 20:54:44 -0600

    tweaks

--- _gh2_xbox.bat
+++ _gh2_xbox.bat
@@ -1,4 +1,10 @@
 git pull https://github.com/hmxmilohax/Guitar-Hero-II-Deluxe-360 main
-call "%~dp0_album_art.bat"
+@echo off
+echo:
+echo:Copying detected album art files to their proper place.
+echo:To add your own, place extracted LIVE files from your packs to \content\415607E7\00000002.
+echo:Building GHIIDX ARK soon.
+call "%~dp0_album_art.bat" >nul 2>&1
+@echo on
 "%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\Xbox\gen" -e -s 4073741823
 pause
--- _gh2_xenia.bat
+++ _gh2_xenia.bat
@@ -1,5 +1,9 @@
 git pull https://github.com/hmxmilohax/Guitar-Hero-II-Deluxe-360 main
-call "%~dp0_album_art.bat"
+@echo off
+echo:
+echo:Copying detected album art files to their proper place. Building GHIIDX ARK soon.
+call "%~dp0_album_art.bat" >nul 2>&1
+@echo on
 "%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\Xbox\gen" -e -s 4073741823
 "%~dp0xenia_canary.exe" "%~dp0\_build\Xbox\default.xex"
 pause
