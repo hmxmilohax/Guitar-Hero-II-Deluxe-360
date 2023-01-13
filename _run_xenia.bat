df5bc0a6191a36ccb169a0a4251903616b188087
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-01-13 10:43:06 -0600

    Update _run_xenia.bat

--- _run_xenia.bat
+++ _run_xenia.bat
@@ -1,2 +1,19 @@
+FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse main`) DO (
+SET localcommit=%%F
+)
+FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse origin/main`) DO (
+SET origincommit=%%F
+)
+echo:local commit = %localcommit% / latest commit = %origincommit%
+IF %localcommit% == %origincommit% (
+IF NOT EXIST "%~dp0_build/Xbox/gen/main_0.ark" (
+echo:ARK not found, building
+call _build_xenia.bat
+) ELSE (
 call _init-dta2b >nul 2>&1
 START "" "%~dp0_xenia\xenia_canary" "%~dp0_build\Xbox\default.xex"
+)
+) ELSE (
+exho:Fetching latest updates and building ARK
+call _build_xenia.bat
+)
