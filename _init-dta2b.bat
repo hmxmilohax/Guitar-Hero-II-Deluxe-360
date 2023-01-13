256c8433e13a46c7dcfe40ab7cccc3ba16428936
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-01-12 23:14:10 -0600

    move inits

---
+++ _init-dta2b.bat
@@ -0,0 +1,10 @@
+"%~dp0dependencies/dtab" -b "%~dp0\_init\init_track_theme.dta" "%~dp0\_init\init_track_theme.dtb"
+"%~dp0dependencies/dtab" -b "%~dp0\_init\init_ui_theme.dta" "%~dp0\_init\init_ui_theme.dtb"
+"%~dp0dependencies/dtab" -b "%~dp0\_init\init.dta" "%~dp0\_init\init.dtb"
+"%~dp0dependencies/dtab" -e "%~dp0\_init\init_track_theme.dtb" "%~dp0\_build\Xbox\gen\init_track_theme.dtb"
+"%~dp0dependencies/dtab" -e "%~dp0\_init\init_ui_theme.dtb" "%~dp0\_build\Xbox\gen\init_ui_theme.dtb"
+"%~dp0dependencies/dtab" -e "%~dp0\_init\init.dtb" "%~dp0\_build\Xbox\gen\init.dtb"
+del "%~dp0\_init\init_ui_theme.dtb"
+del "%~dp0\_init\init_track_theme.dtb"
+del "%~dp0\_init\init.dtb"
+pause
