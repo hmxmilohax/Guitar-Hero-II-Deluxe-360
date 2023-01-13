cebf88372feac38a5f6c569dcb767b249512c9ae
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-01-13 15:40:26 -0600

    solo percentage

--- _ark/ui/gen/_milo2dir_xbox.bat
+++ _ark/ui/gen/_milo2dir_xbox.bat
@@ -1,1 +1,1 @@
-FOR /F "tokens=*" %%G IN ('dir /b poorspy_viewer.milo_xbox') DO superfreq milo2dir "%%G" "unpack_xbox\%%~nG" --convertTextures --preset gh2_x360 --bigEndian
+FOR /F "tokens=*" %%G IN ('dir /b *.milo_xbox') DO superfreq milo2dir "%%G" "unpack_xbox\%%~nG" --convertTextures --preset gh2_x360 --bigEndian
