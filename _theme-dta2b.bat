21ae17246d472fbe59ab6866f70cbaa63e7fab03
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-01-13 08:11:58 -0600

    Fastgh2 (#10)
    * script stash
    
    * fast start xex
    
    * move inits
    
    * oop
    
    * restore vanilla inits
    
    * Update build.yml
    
    * Update build.yml

--- _theme-dta2b.bat
+++
@@ -1,7 +0,0 @@
-"%~dp0dependencies/dtab" -b "%~dp0\_theme\init_track_theme.dta" "%~dp0\_theme\init_track_theme.dtb"
-"%~dp0dependencies/dtab" -b "%~dp0\_theme\init_ui_theme.dta" "%~dp0\_theme\init_ui_theme.dtb"
-"%~dp0dependencies/dtab" -e "%~dp0\_theme\init_track_theme.dtb" "%~dp0\_build\Xbox\gen\init_track_theme.dtb"
-"%~dp0dependencies/dtab" -e "%~dp0\_theme\init_ui_theme.dtb" "%~dp0\_build\Xbox\gen\init_ui_theme.dtb"
-del "%~dp0\_theme\init_ui_theme.dtb"
-del "%~dp0\_theme\init_track_theme.dtb"
-pause
