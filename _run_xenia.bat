729af336a2af182129163f34c08c0c3d7ec778e4
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-01-13 20:00:59 -0600

    fix run xenia?

--- _run_xenia.bat
+++ _run_xenia.bat
@@ -5,11 +5,11 @@ set /p localcommit=<.git\refs\heads\main
 IF EXIST ".git\refs\heads\master" (
 set /p localcommit=<.git\refs\heads\master
 )
-FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse HEAD`) DO (
+FOR /F "tokens=* USEBACKQ" %%F IN (`git ls-remote https://github.com/hmxmilohax/Guitar-Hero-II-Deluxe-360 HEAD`) DO (
 SET origincommit=%%F
 )
-echo:local commit = %localcommit% / latest commit = %origincommit%
-IF %localcommit% == %origincommit% (
+echo:local commit = %localcommit%	HEAD / latest commit = %origincommit%
+IF "%localcommit%	HEAD" == "%origincommit%" (
 IF NOT EXIST "%~dp0_build/Xbox/gen/main_0.ark" (
 echo:ARK not found, building
 call _build_xenia.bat
@@ -18,6 +18,6 @@ call _init-dta2b >nul 2>&1
 START "" "%~dp0_xenia\xenia_canary" "%~dp0_build\Xbox\default.xex"
 )
 ) ELSE (
-exho:Fetching latest updates and building ARK
+echo:Fetching latest updates and building ARK
 call _build_xenia.bat
 )
